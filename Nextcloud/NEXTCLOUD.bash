d9fc016695b1ec876b8464172d23f589f806dcd6
Dixel <63664894+Dixel1@users.noreply.github.com>
2024-04-20 18:31:22 +0200

    Added version 0.6b of the NEXTCLOUD.bash script
    # Changelog for Nextcloud Deployment Script
    
    ## [v0.6b] - 2024-04-20
    ### Added
    - Initial version of the script with basic Nextcloud installation steps.
    - System packages update command.
    - Installation of necessary packages for Nextcloud.
    - Download of the latest Nextcloud release.
    - Unzipping of the downloaded Nextcloud zip file.
    - Creation of a new directory in the web root and copying of Nextcloud files there.
    - Creation of a MySQL database and user for Nextcloud.
    - Configuration of Apache for Nextcloud.
    - Enabling of required Apache modules.
    - Configuration of Apache to redirect requests to the Nextcloud installation.
    - Restart of Apache to apply changes.
    - Printout of a success message after installation.

---
+++ Nextcloud/NEXTCLOUD.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Description: Nextcloud deployment
+# Autor: Dixel - https://github.com/Dixel1
+# Version: v0.6b
+
+# Update the system packages
+sudo apt-get update -y
+
+# Install the necessary packages
+sudo apt-get install apache2 php libapache2-mod-php php-gd php-mysql php-curl php-mbstring php-xml php-zip php-intl php-imagick php-bcmath php-gmp mariadb-server mariadb-client python3-pymysql unzip -y
+
+# Download Nextcloud
+wget https://download.nextcloud.com/server/releases/latest.zip -O nextcloud.zip
+
+# Unzip the zip file
+unzip nextcloud.zip
+
+# Copy the necessary files to the/var/www/html directory
+sudo mkdir /var/www/html/nexcloud
+sudo cp nextcloud /var/www/html/
+
+# Create a MySQL user for Nextcloud
+sudo mysql -u root -e "CREATE DATABASE nextcloud;"
+sudo mysql -u root -e "CREATE USER 'nextcloud_user'@'localhost' IDENTIFIED BY 'nextcloud_password';"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud_user'@'localhost';"
+sudo mysql -u root -e "FLUSH PRIVILEGES;"
+
+# Configure Apache for Nextcloud
+sudo cp /var/www/html/nextcloud/config/config.sample.php /var/www/html/nextcloud/config/config.php
+sudo chown www-data:www-data /var/www/html/nextcloud/config/config.php
+
+# Enable required modules in Apache
+sudo a2enmod rewrite
+sudo service apache2 restart
+
+# Redirect requests to Nextcloud
+echo "Redirect / https://yourdomain.com/nextcloud/" | sudo tee -a /etc/apache2/conf-available/redirect.conf
+sudo a2enconf redirect
+sudo service apache2 restart
+echo "Nextcloud has been installed successfully!"
